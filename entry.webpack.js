85187432566f0827cdbfd7750c9b3c577544f25a
Jed Helmers <jhelmers@miletwo.us>
2020-04-06 20:37:47 -0400

    Create entry.webpack.js

---
+++ entry.webpack.js
@@ -0,0 +1,17 @@
+import pdfjs from 'pdfjs-dist';
+import Document from './src/Document';
+import Outline from './src/Outline';
+import Page from './src/Page'; // eslint-disable-next-line
+
+import PdfjsWorker from 'worker-loader!./src/pdf.worker.entry.js';
+import { isLocalFileSystem, warnOnDev } from './src/shared/utils';
+
+if (isLocalFileSystem) {
+  warnOnDev('You are running React-PDF from your local file system. PDF.js Worker may fail to load due to browser\'s security policies. If you\'re on Google Chrome, you can use --allow-file-access-from-files flag for debugging purposes.');
+}
+
+if (typeof window !== 'undefined' && 'Worker' in window) {
+  pdfjs.GlobalWorkerOptions.workerPort = new PdfjsWorker();
+}
+
+export { pdfjs, Document, Outline, Page };
